9fa120050f32a525d9768625775ad000836ca17f
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-04-04 09:16:04 +0700

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,49 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+N = int(input())
+graph = []
+
+for _ in range(N):
+    graph.append(list(map(int, input().strip())))
+
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+
+def bfs(graph, a, b):
+    n = len(graph)
+    queue = deque()
+    queue.append((a, b))
+    graph[a][b] = 0
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+                count += 1
+
+    return count
+
+
+cnt = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            cnt.append(bfs(graph, i, j))
+
+cnt.sort()
+print(len(cnt))
+for i in range(len(cnt)):
+    print(cnt[i])
