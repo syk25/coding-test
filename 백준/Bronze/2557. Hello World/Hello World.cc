4695e5d2cd0bc0401c57f936c15bc0848babc9c9
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-05-05 14:11:10 +0700

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.cc
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main(){
-
-    string inputString = "Hello World!";
-    cout << inputString << "\n";
+    cout << "Hello World!\n";
 }
