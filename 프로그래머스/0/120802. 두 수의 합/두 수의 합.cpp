655cf4384e450f8cd8ca14aaf48db656835e598a
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-05-05 15:05:55 +0700

    [level 0] Title: 두 수의 합, Time: 0.04 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    ios::sync_with_stdio(0), cin.tie(0);
+    return num1 + num2;
+}
