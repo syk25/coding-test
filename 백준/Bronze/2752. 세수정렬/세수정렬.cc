9c97342934e90f46cb5169c2e532a3be183db856
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-05-05 14:54:02 +0700

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,25 +1,15 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int a, b, c, tmp = 0;
+int main(void) {
+  ios::sync_with_stdio(0), cin.tie(0);
+
+  int a, b, c;
   cin >> a >> b >> c;
-  if (a > b) {
-    tmp = a;
-    a = b;
-    b = tmp;
-  }
-  if (a > c) {
-    tmp = a;
-    a = c;
-    c = tmp;
-  }
-  if (b > c) {
-    tmp = b;
-    b = c;
-    c = tmp;
-  }
-  cout << a << " " << b << " " << c;
-  return 0;
+
+  int d, e, f;
+  d = min({a, b, c});
+  f = max({a, b, c});
+  e = a + b + c - d - f;
+  cout << d << ' ' << e << ' ' << f;
 }
