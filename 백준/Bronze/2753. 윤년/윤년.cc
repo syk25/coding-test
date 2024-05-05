1ca9d8f8e77e087be0dcea1d4fa70dc951cd5923
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-05-05 15:46:56 +0700

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -5,12 +5,8 @@ using namespace std;
 int main(void) {
   int year;
   cin >> year;
-  if (year % 4 == 0) {
-    if (year % 100 != 0 || year % 400 == 0) {
-      cout << 1;
-    } else {
-      cout << 0;
-    }
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    cout << 1;
   } else {
     cout << 0;
   }
