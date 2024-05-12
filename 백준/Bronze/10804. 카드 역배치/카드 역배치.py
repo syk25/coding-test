26e35ebde05fe447d6e1a9687540a88b411f95b2
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-05-12 15:20:57 +0900

    [Bronze II] Title: 카드 역배치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.py
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.py
@@ -1,16 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-def sol():
-    numbers = [x for x in range(21)]
-    for i in range(10):
-        a, b = map(int, input().split())
-        arrange(a, b, numbers)
-    print(*numbers[1:21])
+my_list = [i for i in range(1, 21)]
 
-def arrange(a, b, numbers):
-    reverse = numbers[a:b + 1][::-1]
-    for i in range(a, b + 1):
-        numbers[i] = reverse[i - a]
+for _ in range(10):
+        start, end = map(int, input().split())
+        my_list[start-1:end] = my_list[start-1:end][::-1]
 
-sol()
+print(*my_list)
