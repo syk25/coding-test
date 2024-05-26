9467ab20ee1840453b3ce4e1bddcba5380f5cd40
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-05-26 16:05:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -9,16 +9,27 @@ input = sys.stdin.readline
 
 # 입력받기
 N, K = map(int, input().split())
-q = deque([x for x in range(1, N + 1)])  # 큐
-p = [0] * N  # 순서를 담는 배열
+# q = deque([x for x in range(1, N + 1)])  # 큐
+q = [x for x in range(1, N + 1)]  # 큐
+idx = K - 1
+# p = [0] * N  # 순서를 담는 배열
+p = []
 
 # 요세푸스 순열 구하기
-while len(q) > 1:
-    for _ in range(K - 1):
-        q.append(q.popleft())
-    p[N - len(q) - 1] = q.popleft()
+# while len(q) > 1:
+#     for _ in range(K - 1):
+#         q.append(q.popleft())
+#     p[N - len(q) - 1] = q.popleft()
 
-p[N - 1] = q.popleft()
+# p[N - 1] = q.popleft()
+
+# 두번째 로직
+while(len(p) < N): #0<7, 1 < 7, 2 < 7, 3 < 7, 
+    if idx >= len(q):
+        idx %= len(q)
+    p.append(q.pop(idx))
+    idx += K-1
+
 
 # 출력하기
 print("<", end="")
