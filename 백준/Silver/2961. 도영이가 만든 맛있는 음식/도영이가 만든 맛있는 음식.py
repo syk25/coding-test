3fc17b44708c89d919f598c9a311ce88b306f130
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-06-12 16:12:35 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,17 +1,18 @@
 import sys
+
 input = sys.stdin.readline
 MAX = 1e9
 
 n = int(input())
-ingres = [tuple(map(int, input().split())) for _ in range(n)]
+ingredients = [tuple(map(int, input().split())) for _ in range(n)]
 
 answer = MAX
-for set in range(1, 1 << n):
-  s = 1
-  b = 0
-  for j in range(n):
-    if (set & (1 << j)) != 0:
-      s *= ingres[j][0]
-      b += ingres[j][1]
-  answer = min(answer, abs(s - b))
+for set in range(1, 1<< n):
+    sour = 1
+    bitter = 0
+    for j in range(n):
+        if(set & ( 1<< j)) != 0:
+            sour *= ingredients[j][0]
+            bitter += ingredients[j][1]
+    answer = min(answer, abs(sour - bitter))
 print(answer)
