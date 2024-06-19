5d1031bf21c89c71e05f1441320b1d0999d5bdc4
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-06-19 13:07:05 +0900

    [Silver IV] Title: 서든어택 3, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/22993. 서든어택 3/서든어택 3.py
+++ 백준/Silver/22993. 서든어택 3/서든어택 3.py
@@ -1,10 +1,11 @@
 N = int(input())
 attack = list(map(int, input().strip().split()))
 
+
 def findOut(N, attack):
     junwon = attack[0]
     other_attack = sorted(attack[1:len(attack)])
-    for i in range(0, len(other_attack)):
+    for i in range(0, N-1):
         if junwon > other_attack[i]:
             junwon += other_attack[i]
         elif junwon <= other_attack[i]:
