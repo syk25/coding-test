d80d812984f1b4b6c86cc9947df805a0910abe7d
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-08-15 01:57:02 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,9 +1,1 @@
-word = [x for x in input().strip()]
-
-for i in range(len(word)):
-    if word[i].isupper() == True:
-        word[i] = word[i].lower()
-    else:
-        word[i] = word[i].upper()
-
-print(''.join(word))
+print(input().swapcase())
