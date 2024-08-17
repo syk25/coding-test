0b7f071a35910039947aca55a82dbd3207127135
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-08-17 19:09:03 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,11 +1,7 @@
 tests = int(input())
 my_list = [list(map(int, input().split()))for x in range(tests)] # 층수, 방수, 손님 번호
 
-for test in my_list:
-    height, width, number = test
-    x_count, y_count = 1, 0
-    while number > height:
-        number -= height
-        x_count += 1
-    y_count = number
-    print(str(y_count) + str(x_count).zfill(2))
+for height, width, number in my_list:
+    y_count = number % height if number % height != 0 else height
+    x_count = (number + height - 1) // height
+    print(f"{y_count}{x_count:02}")
