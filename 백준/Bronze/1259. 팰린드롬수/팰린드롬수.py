5c642d846b6995c63ca3ccbd92ee1bb3742edfa8
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-08-19 01:02:52 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,16 +1,7 @@
-my_list = []
-while True:
-    my_list.append(input())
-    if my_list[-1] == "0":
-        break
-
-
-for i in range(len(my_list) - 1):
-    check = True
-    for j in range((len(my_list[i]) // 2) + 1):
-        if my_list[i][j] != my_list[i][len(my_list[i]) - 1 - j]:
-            print("no")
-            check = False
-            break
-    if check:
+this_time = input()
+while this_time != '0':
+    if this_time == this_time[::-1]:
         print("yes")
+    else:
+        print("no")
+    this_time = input()
