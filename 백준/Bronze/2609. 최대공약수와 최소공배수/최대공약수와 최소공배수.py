0a9ed678f5ae173f3b3176cee33b26dbd34946d9
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2024-08-19 01:29:22 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,25 +1,11 @@
 n, m = map(int, input().split())
 
-my_max = max(n, m)
-my_min = min(n, m)
+gcd = 1
 
-"""
-두개의 수 중 큰수를 작은 수로 나눈다
+for i in range(1, max(n,m) + 1):
+    if n % i == 0 and m % i == 0:
+        gcd = i
+lcm = n * m // gcd
 
-나머지가 0이 아닌 경우에 작은 수를 큰수로 갱신하고 나머지를 작은수로 갱신한다
-나머지가 0인 경우에 작은 수가 최대공약수다
-
-"""
-
-r = my_max % my_min
-
-while r != 0:
-    my_max = my_min
-    my_min = r
-    r = my_max % my_min
-
-my_gcd = my_min
-my_lcm = n * m // my_gcd
-
-print(my_gcd)
-print(my_lcm)
+print(gcd)
+print(lcm)
